720b13f20eb18a6cd54a1aa93e1ec23eb9415bd3
YS <jyys98@gmail.com>
2016-01-28 08:46:34 +0800

    待实现cache

---
+++ ZhihuDaily.ApiLib/CacheUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.Storage;
+
+namespace ZhihuDaily.ApiLib
+{
+    public class CachedAttribute : Attribute
+    {
+        public IAsyncOperation<StorageFolder> ImgCacheFolder
+            =>
+                ApplicationData.Current.LocalFolder.CreateFolderAsync("image_cache",
+                    CreationCollisionOption.OpenIfExists);
+
+        public IAsyncOperation<StorageFolder> DataCacheFolder
+            =>
+                ApplicationData.Current.LocalFolder.CreateFolderAsync("data_cache",
+                    CreationCollisionOption.OpenIfExists);
+
+        public CachedAttribute(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum CacheType
+    {
+        Image=0,
+        Data=1
+    }
+}
